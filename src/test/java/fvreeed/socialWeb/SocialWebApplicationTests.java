432e28225c99a04d055ed88efbd476b80daa698c
fvreeed <novuzovfar1d@mail.ru>
2023-07-27 14:34:22 +0300

    first commit

---
+++ src/test/java/fvreeed/socialWeb/SocialWebApplicationTests.java
@@ -0,0 +1,13 @@
+package fvreeed.socialWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
